3e565f9d4869cbdce895922fc220b96576bf46a5
Willy Richardson <willy@brimbox.com>
2014-10-30 17:08:06 -0400

    Security Update and IP column

--- bb-utilities/bb_update.php
+++ bb-utilities/bb_update.php
@@ -16,25 +16,5 @@ You should have received a copy of the GNU GPL v3 along with this program.
 If not, see http://www.gnu.org/licenses/
 */
 
-$main->check_permission(5);
-$xml_version = $main->get_xml($con, "bb_manage_modules");
-
-//@unlink("bb-primary/bb_home_extra.php")
-
-unset($xml_version->database);
-$xml_version->addChild("database","2014.1.22");
-unset($xml_version->program);
-$xml_version->addChild("program","2014.4.397");
-unset($xml_version->backup);
-$xml_version->addChild("backup","2014.1.3");
-
-//change the users_table userrole column to array
-/*
-ALTER TABLE users_table ALTER COLUMN userrole DROP DEFAULT; 
-ALTER TABLE users_table ALTER COLUMN userrole TYPE smallint[] USING array[userrole]::smallint[]; 
-ALTER TABLE users_table ALTER COLUMN userrole SET DEFAULT '{0}';
-ALTER TABLE users_table RENAME COLUMN userrole TO userroles;
-*/
- 
-$main->update_xml($con, $xml_version, "bb_manage_modules");
+$main->check_permission("bb_brimbox", 5);
 ?>
