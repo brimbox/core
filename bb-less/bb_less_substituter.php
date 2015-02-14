7e9173d447a8b7e557451da6b94c79e382ff4908
Willy Richardson <willy@brimbox.com>
2015-02-14 14:00:15 -0500

    LESS parser update

--- bb-less/bb_less_substituter.php
+++ bb-less/bb_less_substituter.php
@@ -18,15 +18,28 @@ If not, see http://www.gnu.org/licenses/
 /* This is a LESS type compiler that works just with variables for substitutions and imports of LESS files */
 /* This is useful when you just want variable substituions and don't need the rest of the LESS specification */
 /* This DOES NOT fully compile LESS and reads the whole LESS file being rendered in one string (careful with large files) */
-/* This will allow imports of LESS variable files and renders the variables and substitutions in order declared*/
+/* This will allow imports of LESS variable files and renders the variables and substitutions in order declared */
 /* This removes comments and whitespace rendering CSS as open styles for inclusion between style tags (not a CSS file) */
 /* Dies on 4 errors, bad file extension, bad file or import statement, bad variable name, or empty variable value */
 
 class bb_less_substituter {
     
+    //main function
     function parse_less_file($filename)
         {
         //get primary LESS file
+        $arr_css = $this->load($filename);
+        //add eol for ending
+        array_push($arr_css, "\n");
+        $i = 0;
+        
+        //descend into recursion
+        $this->recurse($arr_css, $arr_trans, $i);
+        }
+        
+    //load file into string and explode into array    
+    protected function load($filename)
+        {
         $file_parts = pathinfo($filename);
         if (strcasecmp($file_parts['extension'],"less"))
             {
@@ -40,18 +53,14 @@ class bb_less_substituter {
         //remove comments
         $file_string = preg_replace('!/\*.*?\*/!s', '', $file_string);
         //turn into tokens
-        $arr_css = explode(";", $file_string);
-        $arr_css = array_map('trim', $arr_css);
-        $arr_css = array_filter($arr_css);
-        //add eol for ending
-        array_push($arr_css, "\n");
-        $i = 0;
-        
-        //descend into recursion
-        $this->normal($arr_css, $arr_trans, $i);
+        $arr_less = explode(";", $file_string);
+        $arr_less = array_map('trim', $arr_less);
+        $arr_less = array_filter($arr_less);
+        return $arr_less;
         }
-        
-    function normal(&$arr_css, &$arr_trans, &$i)
+    
+    //recurse and parse file substitutiing, importing and outputing     
+    protected function recurse(&$arr_css, &$arr_trans, &$i)
         {
         $next = $arr_css[$i];
         $i++;
@@ -59,23 +68,9 @@ class bb_less_substituter {
         if  (substr($next,0,7) == "@import")
             {
             preg_match('/"([^"]+)"/', $next, $file_array);
-            $file_parts = pathinfo($file_array[1]);
-            if (strcasecmp($file_parts['extension'],"less"))
-                {
-                die("File must have LESS (.less) extension. ($file_array[1])");    
-                }
-            $file_string = file_get_contents($file_array[1]);
-            if (!$file_string)
-                {
-                die("Missing LESS Variable File -- Import line error. ($file_array[1])");
-                }
-            //splice in import
-            $file_string = preg_replace('!/\*.*?\*/!s', '', $file_string);
-            $arr_splice = explode(";", $file_string);
-            $arr_splice = array_map('trim', $arr_splice);
-            $arr_splice = array_filter($arr_splice);
+            $arr_splice = $this->load($file_array[1]);
             array_splice($arr_css,$i+1,0,$arr_splice);
-            $this->normal($arr_css, $arr_trans, $i);   
+            $this->recurse($arr_css, $arr_trans, $i);   
             }
         //variable
         elseif (substr($next,0,1) == "@")
@@ -91,14 +86,14 @@ class bb_less_substituter {
                 die("Empty LESS variable value. ($arr_vars[1])");     
                 }
             $arr_trans[$arr_vars[0]] = $arr_vars[1];
-            $this->normal($arr_css, $arr_trans, $i);
+            $this->recurse($arr_css, $arr_trans, $i);
             }
         //substitution
         elseif ($next <> "\n")
             {
             $next = strtr($next, $arr_trans);
             echo $next . ";";
-            $this->normal($arr_css, $arr_trans, $i);
+            $this->recurse($arr_css, $arr_trans, $i);
             }
         //sucessful parse
         else
