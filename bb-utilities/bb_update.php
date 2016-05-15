43d9d51715e203db18a79efdac437760ab46689a
Willy Richardson <willy@brimbox.com>
2016-05-14 23:12:50 -0400

    V2 Added update.php

---
+++ bb-utilities/bb_update.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * Copyright (C) Brimbox LLC
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License Version 3 (“GNU GPL v3”)
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU GPL v3 for more details.
+ *
+ * You should have received a copy of the GNU GPL v3 along with this program.
+ * If not, see http://www.gnu.org/licenses/
+ */
+?>
