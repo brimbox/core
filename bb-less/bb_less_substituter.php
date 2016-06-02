603cb378a14181b78798920e8676ffe7cb7bffd7
contact1 <contact1@a2s88.a2hosting.com>
2016-06-01 21:07:00 -0400

    V2 Forgot Less Compiler

--- bb-less/bb_less_substituter.php
+++ bb-less/bb_less_substituter.php
@@ -1,10 +1,9 @@
 <?php
-
 /*
  * Copyright (C) Brimbox LLC
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License Version 3 (“GNU GPL v3”)
+ * it under the terms of the GNU General Public License Version 3 (ï¿½GNU GPL v3ï¿½)
  * as published by the Free Software Foundation.
  *
  * This program is distributed in the hope that it will be useful,
@@ -14,7 +13,7 @@
  *
  * You should have received a copy of the GNU GPL v3 along with this program.
  * If not, see http://www.gnu.org/licenses/
- */
+*/
 
 /* This is a LESS type compiler that works just with variables for substitutions and imports of LESS files */
 /* This is useful when you just want variable substituions and don't need the rest of the LESS specification */
@@ -23,74 +22,85 @@
 /* This removes comments and whitespace rendering CSS as open styles for inclusion between style tags (not a CSS file) */
 /* Dies on 4 errors, bad file extension, bad file or import statement, bad variable name, or empty variable value */
 class bb_less_substituter {
-	
-	// main function
-	function parse_less_file($filename) {
-		// get primary LESS file
-		$arr_css = $this->load ( $filename );
-		// add eol for ending
-		array_push ( $arr_css, "\n" );
-		$i = 0;
-		
-		// descend into recursion
-		echo "<style>";
-		$this->recurse ( $arr_css, $arr_trans, $i );
-		echo "</style>";
-	}
-	
-	// load file into string and explode into array
-	protected function load($filename) {
 
-		$file_parts = pathinfo ( $filename );
-		if (strcasecmp ( $file_parts ['extension'], "less" )) {
-			die ( "File must have LESS (.less) extension" );
-		}
-		$file_string = file_get_contents ( $filename );
-		if (! $file_string) {
-			die ( "Missing LESS Variable File" );
-		}
-		// remove comments
-		$file_string = preg_replace ( '!/\*.*?\*/!s', '', $file_string );
-		// turn into tokens
-		$arr_less = explode ( ";", $file_string );
-		$arr_less = array_map ( 'trim', $arr_less );
-		$arr_less = array_filter ( $arr_less );
-		return $arr_less;
-	}
-	
-	// recurse and parse file substitutiing, importing and outputing
-	protected function recurse(&$arr_css, &$arr_trans, &$i) {
+    // main function
+    function parse_less_file($infile, $outfile) {
 
-		$next = $arr_css [$i];
-		$i ++;
-		// import line
-		if (substr ( $next, 0, 7 ) == "@import") {
-			preg_match ( '/"([^"]+)"/', $next, $file_array );
-			$arr_splice = $this->load ( $file_array [1] );
-			array_splice ( $arr_css, $i + 1, 0, $arr_splice );
-			$this->recurse ( $arr_css, $arr_trans, $i );
-		} // variable
-elseif (substr ( $next, 0, 1 ) == "@") {
-			$arr_vars = explode ( ":", $next, 2 );
-			$arr_vars = array_map ( 'trim', $arr_vars );
-			if (preg_match ( "/[^@A-Za-z0-9_-]/", $arr_vars [0] )) {
-				die ( "Invalid LESS character(s) in variable name. ($arr_vars[0])" );
-			}
-			if (empty ( $arr_vars [1] ) && ($arr_vars [1] !== "0")) {
-				die ( "Empty LESS variable value. ($arr_vars[1])" );
-			}
-			$arr_trans [$arr_vars [0]] = $arr_vars [1];
-			$this->recurse ( $arr_css, $arr_trans, $i );
-		} // substitution
-elseif ($next != "\n") {
-			$next = strtr ( $next, $arr_trans );
-			echo $next . ";";
-			$this->recurse ( $arr_css, $arr_trans, $i );
-		}  // sucessful parse
-else {
-			return false;
-		}
-	}
+        // get primary LESS file
+        @$file_string = file_get_contents($infile);
+        if (!$file_string) die("Cannot find file: " . $infile);
+        $arr_file = preg_split("/(\r\n|\n|\r)/", $file_string);
+
+        // parse primary LESS file
+        $arr_stripped = $this->strip($file_string);
+        // add eol for ending
+        array_push($arr_stripped, "\n");
+        $i = 0;
+
+        // descend into recursion to get array_trans
+        $this->recurse($arr_stripped, $arr_less_vars, $i);
+
+        //substitute, eliminate and parse less file
+        $j = 0;
+        $output = "";
+        foreach ($arr_file as $string) {
+            if (((substr(trim($string), 0, 1) <> "@") || (substr(trim($string), 0, 6) == "@media"))) {
+                $string = str_replace(array_keys($arr_less_vars), array_values($arr_less_vars), $string);
+                if (trim($string) == "") {
+                    $j++;
+                }
+                if (($j < 2) || (trim($string) <> "")) {
+                    $output.= $string . "\r\n";
+                    if (trim($string) <> "") {
+                        $j = 0;
+                    }
+
+                }
+            }
+        }
+        //write new css file
+        file_put_contents($outfile, $output);
+    }
+
+    // load file into string and explode into array
+    protected function strip($file_string) {
+
+        // remove comments
+        $file_string = preg_replace('!/\*.*?\*/!s', '', $file_string);
+        // turn into tokens
+        $arr_exploded = explode(";", $file_string);
+        $arr_exploded = array_map('trim', $arr_exploded);
+        $arr_exploded = array_filter($arr_exploded);
+        return $arr_exploded;
+    }
+
+    // recurse and parse file substitutiing, importing and outputing
+    protected function recurse(&$arr_stripped, &$arr_less_vars, &$i) {
+
+        $next = $arr_stripped[$i];
+        $i++;
+        // import line
+        if (substr($next, 0, 7) == "@import") {
+            preg_match('/"([^"]+)"/', $next, $file_array);
+            @$file_string = file_get_contents($file_array[1]);
+            if (!$file_string) die("Cannot find @import file: " . $file_array[1]);
+            $arr_splice = $this->strip($file_string);
+            array_splice($arr_stripped, $i + 1, 0, $arr_splice);
+            $this->recurse($arr_stripped, $arr_less_vars, $i);
+        }
+        elseif (substr($next, 0, 1) == "@") {
+            $arr_vars = explode(":", $next, 2);
+            $arr_vars = array_map('trim', $arr_vars);
+            $arr_less_vars[$arr_vars[0]] = $arr_vars[1];
+            $this->recurse($arr_stripped, $arr_less_vars, $i);
+        }
+        elseif ($next != "\n") {
+            $this->recurse($arr_stripped, $arr_less_vars, $i);
+        }
+        else {
+            return false;
+        }
+    }
 } // end class
 
 ?>
